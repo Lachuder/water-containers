e39d304bc48292db38bafb54609ff9d878c61d01
Adrian Debek <adryjano@gmail.com>
2025-06-03 20:59:08 +0200

    Added addWater()

---
+++ src/main/java/pl/kurs/watercontainers/exceptions/InvalidWaterAmountException.java
@@ -0,0 +1,8 @@
+package pl.kurs.watercontainers.exceptions;
+
+public class InvalidWaterAmountException extends RuntimeException {
+
+    public InvalidWaterAmountException(String message) {
+        super(message);
+    }
+}
