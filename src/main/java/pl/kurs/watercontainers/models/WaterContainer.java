bb0289fd80b6eea46c5a8318986ade5ef3bad34c
Michal Sloniewski <michal.sloniewski@smartcodecreations.com>
2025-06-17 20:01:34 +0200

    changed return types from void to boolean in basic WaterContainer operations methods

--- src/main/java/pl/kurs/watercontainers/models/WaterContainer.java
+++ src/main/java/pl/kurs/watercontainers/models/WaterContainer.java
@@ -17,51 +17,67 @@ public class WaterContainer implements Serializable {
         this.waterLevel = waterLevel;
     }
 
-    public void addWater(double value) {
+    public boolean addWater(double value) {
         if (value <= 0) {
-            throw new InvalidWaterAmountException("Value should by more than 0");
+            System.out.println("Value should by more than 0");
+            return false;
         }
         if (waterLevel + value > maxCapacity) {
-            throw new InvalidWaterAmountException("Too much water to add");
+            System.out.println("Too much water to add");
+            return false;
         }
         waterLevel += value;
+        return true;
     }
 
-    public void subtractWater(double value) {
+    public boolean subtractWater(double value) {
         if (value <= 0) {
-            throw new InvalidWaterAmountException("Value should be more than 0");
+            System.out.println("Value should be more than 0");
+            return false;
         }
         if (waterLevel - value < 0) {
-            throw new InvalidWaterAmountException("Too much water to subtract");
+            System.out.println("Too much water to subtract");
+            return false;
         }
         waterLevel -= value;
+        return true;
     }
-    public void pourWater(WaterContainer destinationContainer, double value) {
+
+    public boolean pourWater(WaterContainer destinationContainer, double value) {
         if (destinationContainer == null) {
-            throw new IllegalArgumentException("Destination container can not be null");
+            System.out.println("Destination container can not be null");
+            return false;
         }
         if (value <= 0) {
-            throw new InvalidWaterAmountException("Valude should be more than 0");
+            System.out.println("Valude should be more than 0");
+            return false;
         }
-        this.subtractWater(value);
-        destinationContainer.addWater(value);
+        boolean operation1 = this.subtractWater(value);
+        boolean operation2 = destinationContainer.addWater(value);
+        return operation1 && operation2;
+
     }
 
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public double getMaxCapacity() {
         return maxCapacity;
     }
+
     public void setMaxCapacity(double maxCapacity) {
         this.maxCapacity = maxCapacity;
     }
+
     public double getWaterLevel() {
         return waterLevel;
     }
+
     public void setWaterLevel(double waterLevel) {
         this.waterLevel = waterLevel;
     }
@@ -73,6 +89,7 @@ public class WaterContainer implements Serializable {
         WaterContainer that = (WaterContainer) o;
         return Double.compare(maxCapacity, that.maxCapacity) == 0 && Double.compare(waterLevel, that.waterLevel) == 0 && Objects.equals(name, that.name);
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(name, maxCapacity, waterLevel);
