7dccbbec7f8234bb50d57ec37d484a0c96c03be6
Adrian Debek <adryjano@gmail.com>
2025-07-08 20:54:45 +0200

    basic methods added to Car class

--- src/main/java/pl/kurs/watercontainers/models/Car.java
+++ src/main/java/pl/kurs/watercontainers/models/Car.java
@@ -1,5 +1,7 @@
 package pl.kurs.watercontainers.models;
 
+import java.util.Objects;
+
 public class Car {
 
     private String name;
@@ -12,4 +14,44 @@ public class Car {
         this.productionYear = productionYear;
     }
 
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public int getProductionYear() {
+        return productionYear;
+    }
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return productionYear == car.productionYear && Objects.equals(name, car.name) && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, model, productionYear);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", model='" + model + '\'' +
+                ", productionYear=" + productionYear +
+                '}';
+    }
 }
