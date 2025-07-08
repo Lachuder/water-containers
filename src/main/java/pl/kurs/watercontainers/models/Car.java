cd3b9a3e41e8408a0440044e5f6f144de7626cbd
Adrian Debek <adryjano@gmail.com>
2025-07-08 20:53:07 +0200

    Car class added

---
+++ src/main/java/pl/kurs/watercontainers/models/Car.java
@@ -0,0 +1,15 @@
+package pl.kurs.watercontainers.models;
+
+public class Car {
+
+    private String name;
+    private String model;
+    private int productionYear;
+
+    public Car(String name, String model, int productionYear) {
+        this.name = name;
+        this.model = model;
+        this.productionYear = productionYear;
+    }
+
+}
