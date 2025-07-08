12ba942cb8ec68d0c889dd7e6ac5a99222b7dbaf
Adrian Debek <adryjano@gmail.com>
2025-07-08 20:07:02 +0200

    addWater() and subtractWater() updated with logging

--- src/main/java/pl/kurs/watercontainers/models/WaterContainer.java
+++ src/main/java/pl/kurs/watercontainers/models/WaterContainer.java
@@ -3,13 +3,19 @@ package pl.kurs.watercontainers.models;
 import pl.kurs.watercontainers.exceptions.InvalidWaterAmountException;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class WaterContainer implements Serializable {
     private static final long serialVersionUID = 1L;
+
     private String name;
     private double maxCapacity;
     private double waterLevel;
+    private List<OperationLog> logs = new ArrayList<>();
 
     public WaterContainer(String name, double maxCapacity, double waterLevel) {
         this.name = name;
@@ -18,27 +24,33 @@ public class WaterContainer implements Serializable {
     }
 
     public boolean addWater(double value) {
+        boolean success;
         if (value <= 0) {
             System.out.println("Value should by more than 0");
-            return false;
+            success = false;
+        } else if (!addIsPossible(value)) {
+            success = false;
+        } else {
+            waterLevel += value;
+            success = true;
         }
-        if (!addIsPossible(value)) {
-            return false;
-        }
-        waterLevel += value;
-        return true;
+        logs.add(new OperationLog(Timestamp.from(Instant.now()), OperationLog.OperationType.ADD, this, value, success));
+        return success;
     }
 
     public boolean subtractWater(double value) {
+        boolean success;
         if (value <= 0) {
             System.out.println("Value should be more than 0");
-            return false;
+            success = false;
+        } else if (!subtractIsPossible(value)) {
+            success = false;
+        } else {
+            waterLevel -= value;
+            success = true;
         }
-        if (!subtractIsPossible(value)) {
-            return false;
-        }
-        waterLevel -= value;
-        return true;
+        logs.add(new OperationLog(Timestamp.from(Instant.now()), OperationLog.OperationType.ADD, this, value, success));
+        return success;
     }
 
     public boolean pourWater(WaterContainer destinationContainer, double value) {
@@ -47,7 +59,7 @@ public class WaterContainer implements Serializable {
             return false;
         }
         if (value <= 0) {
-            System.out.println("Valude should be more than 0");
+            System.out.println("Value should be more than 0");
             return false;
         }
         if (!this.subtractIsPossible(value) || !destinationContainer.addIsPossible(value)) {
@@ -78,23 +90,18 @@ public class WaterContainer implements Serializable {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public double getMaxCapacity() {
         return maxCapacity;
     }
-
     public void setMaxCapacity(double maxCapacity) {
         this.maxCapacity = maxCapacity;
     }
-
     public double getWaterLevel() {
         return waterLevel;
     }
-
     public void setWaterLevel(double waterLevel) {
         this.waterLevel = waterLevel;
     }
